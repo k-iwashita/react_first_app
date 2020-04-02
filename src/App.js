d5aaf5fc1b23841f4aa5b75f3dced46a6ef29829
k-iwashita <k.iwashita.gude@gmail.com>
2020-04-02 22:07:33 +0900

    learn components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render() {
-    return(
-      <React.Fragment>
-        <label>kkkk</label>
-        <input type="text" onClick={ () => {console.log('kensho')}}></input>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label>kkkk</label>
+//         <input type="text" onClick={ () => {console.log('kensho')}}></input>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+
+  )
+}
+
+const Cat = () => {
+  return <div>hi!</div>
 }
 
 export default App;
